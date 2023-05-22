b65638dfad15e2e4c6dc91b64aa848b5f6c3eb8c
winwin71 <winattack@hotmail.com>
2023-05-22 17:11:41 +0700

    CreateActError400

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -5,22 +5,21 @@ import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
 
 function App() {
-  const [activities, setActivities] = useState<any>();
+  const [activities, setActivities] =useState([]);
 
-  useEffect(() => {
+  useEffect(() =>{
     axios.get('http://localhost:5000/api/activities')
     .then(response => {
-      console.log(response);
+      console.log(response)
       setActivities(response.data);
     })
-  }, [])
-
+  },[])
   return (
     <div>
-      <Header as='h2' icon='users' content='Reactivite' />
+        <Header as='h2' icon='users' content='Reactivities'/>
         <List>
           {activities.map((activity: any) => (
-            <List.Item key={activity.id}>
+            <List.Item key = {activity.id}>
               {activity.title}
             </List.Item>
           ))}
